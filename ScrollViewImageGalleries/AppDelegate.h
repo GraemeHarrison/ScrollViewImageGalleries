3ba39497411216de88f32308a145bfdb62e15356
Graeme Harrison <gharrison365@gmail.com>
2016-01-25 11:04:38 -0500

    Initial commit

---
+++ ScrollViewImageGalleries/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewImageGalleries
+//
+//  Created by Graeme Harrison on 2016-01-25.
+//  Copyright © 2016 Graeme Harrison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
